54d209f2baf31618efe445b053af59323452dcdb
Githubb8888 <150154786+Githubb8888@users.noreply.github.com>
2026-09-26 12:24:34 +0700

    Delete TP2DPBO2526C1 directory

--- TP2DPBO2526C1/Python/benua.py
+++
@@ -1,5 +0,0 @@
-class Benua:
-    def __init__(self, nama_benua, luas_benua, zona_waktu_utama):
-        self.nama_benua = nama_benua
-        self.luas_benua = luas_benua
-        self.zona_waktu_utama = zona_waktu_utama
--- TP2DPBO2526C1/Python/negara.py
+++
@@ -1,9 +0,0 @@
-from benua import Benua
-
-
-class Negara(Benua):
-    def __init__(self, nama_benua, luas_benua, zona_waktu_utama, nama_negara, bahasa_resmi, mata_uang):
-        super().__init__(nama_benua, luas_benua, zona_waktu_utama)
-        self.nama_negara = nama_negara
-        self.bahasa_resmi = bahasa_resmi
-        self.mata_uang = mata_uang
